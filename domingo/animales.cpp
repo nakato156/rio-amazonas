9a776bf3ee78b30caba6e75cc29fb51351952090
nakato156 <christianvelasces@gmail.com>
2022-09-25 18:00:26 -0500

    beta 1.0

---
+++ domingo/animales.cpp
@@ -0,0 +1,205 @@
+#include "pch.h"
+#include <iostream>
+#include <time.h>
+#include <conio.h>
+
+#define LIMITE 98
+
+using namespace std;
+using namespace System;
+
+class Amazonas {
+protected:
+    int x, y;
+    int velocidad;
+    int posicionX = 0, posicionY = 0;
+    int avanza = 1 ? rand() % 2 : -1;
+
+public:
+    Amazonas() {}
+    ~Amazonas() {}
+
+};
+
+class Cocodrilo : public Amazonas {
+public:
+    Cocodrilo() {
+        velocidad = 1 + rand() % 8; // tamanio salto horizontal
+        x = 1;
+        y = 2 + rand() % 20;
+    }
+    ~Cocodrilo() {}
+
+    void pintaCocodrilo()
+    {
+        Console::SetCursorPosition(x, y);
+        cout << "   _____";
+        Console::SetCursorPosition(x, y + 1);
+        cout << "| | ____=====";
+        Console::SetCursorPosition(x, y + 2);
+        cout << "   /   \\";
+    }
+    void borraCocodrilo()
+    {
+        Console::SetCursorPosition(x, y);
+        cout << "        ";
+        Console::SetCursorPosition(x, y + 1);
+        cout << "             ";
+        Console::SetCursorPosition(x, y + 2);
+        cout << "         ";
+    }
+    void saltoHorizontalCocodrilo() {
+        if (x + velocidad < 0 || x + velocidad > LIMITE) return;
+        x += velocidad;
+    }
+    void eliminaCocodrilo()
+    {
+        if (x > 95)
+        {
+            borraCocodrilo();
+        }
+    }
+    void crearCocodrilo()
+    {
+        borraCocodrilo();
+        saltoHorizontalCocodrilo();
+        pintaCocodrilo();
+        eliminaCocodrilo();
+    }
+};
+
+class Boa : public Amazonas {
+public:
+    Boa() {
+        velocidad = 1 + rand() % 8; // tamanio salto horizontal
+        x = 99;
+        y = 2 + rand() % 20;
+    }
+    ~Boa() {}
+    bool dibujar = true;
+    void pintaBoa() {
+        if (dibujar) {
+            Console::SetCursorPosition(x, y);
+            cout << " O__O ";
+            Console::SetCursorPosition(x, y + 1);
+            cout << "     |";
+            Console::SetCursorPosition(x, y + 2);
+            cout << "     |__";
+        }
+    }
+    void borraBoa() {
+        Console::SetCursorPosition(x, y);
+        cout << "      ";
+        Console::SetCursorPosition(x, y + 1);
+        cout << "      ";
+        Console::SetCursorPosition(x, y + 2);
+        cout << "        ";
+        Console::SetCursorPosition(x, y + 3);
+        cout << "             ";
+    }
+    void saltoHorizontalBoa() {
+        if (x + velocidad <= 3 || x + velocidad > LIMITE) {
+            dibujar = false;
+            return borraBoa();
+        }
+        x += velocidad;
+    }
+    void eliminarBoa()
+    {
+        if (x <= 3) {
+            borraBoa();
+        }
+    }
+    void creaBoa() {
+        borraBoa();
+        saltoHorizontalBoa();
+        pintaBoa();
+        eliminarBoa();
+    }
+};
+
+class Tiburon : public Amazonas {
+public:
+    Tiburon() {
+        velocidad = 1 + rand() % 8; // tamanio salto horizontal
+        int opcion = 1 + rand() % 2;
+        
+        x = opcion ? 90 : 1;
+        if (opcion == 90) velocidad = -1 * velocidad;
+        y = 2 + rand() % 20;
+
+    }
+    bool dibujar = true;
+    void pintaTiburon() {
+        if (dibujar) {
+            Console::SetCursorPosition(x, y);
+            cout << "      __)\\_  ";
+            Console::SetCursorPosition(x, y + 1);
+            cout << "(\\_.-'    a`-.";
+            Console::SetCursorPosition(x, y + 2);
+            cout << "( /~~````(/~^^`";
+        }
+    }
+    void borraTiburon() {
+        Console::SetCursorPosition(x, y);
+        cout << "               ";
+        Console::SetCursorPosition(x, y + 1);
+        cout << "                ";
+        Console::SetCursorPosition(x, y + 2);
+        cout << "                ";
+    }
+    void saltoHorizontalTiburon() {
+        if (x +  velocidad < 0 || x + velocidad > LIMITE) {
+            dibujar = false;
+            return borraTiburon();
+        }
+        x += velocidad;
+    }
+    void eliminaTiburon()
+    {
+        if (x < 5 || x > LIMITE) {
+            borraTiburon();
+        }
+    }
+    void creaTiburon()
+    {
+        borraTiburon();
+        saltoHorizontalTiburon();
+        pintaTiburon();
+        eliminaTiburon();
+    }
+};
+
+template <typename T>
+class Arreglo {
+public:
+    int length = 0;
+    T** arreglo = new T * [1];
+
+    void append(T* elemento) {
+        if (length == 0) arreglo[0] = elemento;
+        else {
+            T** nuevo_arr = new T * [length + 1];
+            for (int i = 0; i < length; i++) nuevo_arr[i] = arreglo[i];
+            nuevo_arr[length + 1] = elemento;
+            arreglo = nuevo_arr;
+        }
+        length++;
+    }
+
+    void pop() {
+        T** nuevo_arr = new T * [length - 1];
+        for (int i = 0; i < length - 1; i++) nuevo_arr[i] = arreglo[i];
+        arreglo = nuevo_arr;
+    }
+
+    void remove(int pos = 0) {
+        T** nuevo_arr = new T * [length - 1];
+        for (int i = 0; i < length - 1; i++) {
+            if (i == pos) continue;
+            nuevo_arr[i] = arreglo[i];
+        }
+        arreglo = nuevo_arr;
+    }
+    T* operator [](unsigned int index) { return arreglo[index]; }
+};
---
+++ domingo/main.cpp
@@ -0,0 +1,103 @@
+﻿#include "pch.h"
+#include "animales.cpp"
+#include <iostream>
+#include <conio.h>
+#include <thread>
+
+#define WIDTH_CONSOLE 100
+#define HEIGHT_CONSOLE 24
+#define ARRIBA 72
+#define AVANZAR 77
+#define ABAJO 80
+#define RETROCEDER 75
+
+using namespace std;
+using namespace System;
+
+class Nadador {
+private:
+    char repr = char(219);
+    int color = 2;
+    int posicionX = 49, posicionY = 23;
+public:
+    void mover(int x, int y) {
+        if (posicionX + x < 0 || posicionY + y < 0) return;
+        else if (posicionX + x > WIDTH_CONSOLE || posicionY + y > HEIGHT_CONSOLE) return;
+        Console::SetCursorPosition(posicionX, posicionY);
+        cout << " ";
+        posicionX += x;
+        posicionY += y;
+    }
+
+    ~Nadador() {
+        cout << "Ha terminado el juego";
+        exit(EXIT_SUCCESS);
+    }
+    friend ostream& operator<<(ostream& os, const Nadador& obj) {
+        Console::SetCursorPosition(obj.posicionX, obj.posicionY);
+        Console::ForegroundColor = ConsoleColor(obj.color);
+        os << obj.repr;
+        Console::ForegroundColor = ConsoleColor(7);
+        return os;
+    }
+};
+
+
+class Game {
+public:
+    void init() {
+        // Se inician los hilos de los animales
+
+        // se crea al nadador
+        Nadador player;
+        cout << player << endl;
+
+        Cocodrilo* cocodrilo = new Cocodrilo();
+        Boa* boa = new Boa();
+        Tiburon* tiburon = new Tiburon();
+
+        Arreglo <Cocodrilo> coleccion_cocodrilo;
+        Arreglo <Boa> coleccion_boa;
+        Arreglo <Tiburon> coleccion_tiburon;
+
+        while (true)
+        {
+            if (kbhit()) {
+                char tecla = getch();
+                if (tecla == AVANZAR) player.mover(1, 0);
+                else if (tecla == RETROCEDER) player.mover(-1, 0);
+                else if (tecla == ARRIBA) player.mover(0, -1);
+                else if (tecla == ABAJO) player.mover(0, 1);
+                else if (tecla == 't') {
+                    coleccion_tiburon.append(new Tiburon());
+                }
+                else if (tecla == 'c') {
+                    coleccion_cocodrilo.append(new Cocodrilo());
+                }
+                else if (tecla == 'b') {
+                    coleccion_boa.append(new Boa());
+                }
+
+                for (int i = 0; i < coleccion_cocodrilo.length; i++) coleccion_cocodrilo[i]->crearCocodrilo();
+                for (int i = 0; i < coleccion_boa.length; i++) coleccion_boa[i]->creaBoa();
+                for (int i = 0; i < coleccion_tiburon.length; i++) coleccion_tiburon[i]->pintaTiburon();
+                
+                cout << player;
+
+                // comprobar colision
+            }
+            _sleep(100);
+        }
+    }
+};
+
+int main() {
+    srand(time(NULL));
+    Console::SetWindowSize(WIDTH_CONSOLE, HEIGHT_CONSOLE);
+    Console::CursorVisible = 0;
+    
+    Game juego;
+    juego.init();
+    
+    return 0;
+}
